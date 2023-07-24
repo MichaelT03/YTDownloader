16e8fdefe9c1322ddad7ac56c2d8a75bec1ec619
Michael Trout <michaeltrout163@gmail.com>
2023-07-24 16:54:19 -0500

    Added tkinter and ttkboostrap for dark theme

--- YTDownloadImp.py
+++ YTDownloadImp.py
@@ -1,2 +1,10 @@
 from YTDownloadSrc import *
+import tkinter as tk
+import ttkbootstrap as ttk
 
+# Window
+window = ttk.Window(themename = "darkly")
+window.title("YouTube Video Downloader")
+
+# Run
+window.mainloop()
