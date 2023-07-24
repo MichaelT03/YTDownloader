f466de4aaa8af0bd6d63026c821687895954ae74
Michael Trout <michaeltrout163@gmail.com>
2023-07-24 18:36:44 -0500

    Moved everything into one file and added input fields
    Also added basic test functionality for input fields

--- YTDownload.py
+++ YTDownload.py
@@ -1,6 +1,11 @@
-from YTDownloadSrc import *
 import tkinter as tk
 import ttkbootstrap as ttk
+from pytube import YouTube
+
+# Functions
+def downloadButtonClick():
+    urlString = url.get()
+    print(urlString)
 
 # Window
 window = ttk.Window(themename = "darkly")
@@ -12,6 +17,16 @@ heading1 = ttk.Label(master = window, text = "Paste a Youtube URL into the text 
 heading1.pack(pady = 10)
 heading2 = ttk.Label(master = window, text = "and select the file type you would like to download", font = "Calibri 24")
 heading2.pack()
+
+# Inputs
+inputFrame = ttk.Frame(master = window)
+url = tk.StringVar()
+textBox = ttk.Entry(master = inputFrame, textvariable = url)
+downloadButton = ttk.Button(master = inputFrame, text = "Download", command = downloadButtonClick)
+
+textBox.pack(side = "left", padx = 10)
+downloadButton.pack(side = "left")
+inputFrame.pack(pady = 5)
 
 # Run
 window.mainloop()
