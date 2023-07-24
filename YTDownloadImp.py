32da6e2e79e09260d05be5cf1cc56b13aba11633
Michael Trout <michaeltrout163@gmail.com>
2023-07-24 18:14:36 -0500

    Set a static window size and created the heading

--- YTDownloadImp.py
+++ YTDownloadImp.py
@@ -5,6 +5,13 @@ import ttkbootstrap as ttk
 # Window
 window = ttk.Window(themename = "darkly")
 window.title("YouTube Video Downloader")
+window.geometry("800x500")
+
+# Heading
+heading1 = ttk.Label(master = window, text = "Paste a Youtube URL into the text field", font = "Calibri 24")
+heading1.pack(pady = 10)
+heading2 = ttk.Label(master = window, text = "and select the file type you would like to download", font = "Calibri 24")
+heading2.pack()
 
 # Run
 window.mainloop()
